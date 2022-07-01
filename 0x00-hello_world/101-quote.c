b1ce3af84a9b4cc4eda8af0ebc771ad8953be2d8
kennywindis <kenyphilip100@gmail.com>
2022-07-01 08:46:43 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - prints exactly "and that piece2 of art is useful"
+* - Dora korpar, 2015-10-19",
+* followed by a new line, to the standard erro.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+write(2, "and that piece of art is useful\* - Dora korpar, 2015-10-19\n", 59);
+
+return (1);
+}
