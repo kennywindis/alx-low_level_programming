421eae9b7b301c497166f99c73e0208f0904dcf7
kennywindis <kenyphilip100@gmail.com>
2022-07-01 09:57:57 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include<unistd.h>
 
 /**
-* main - prints exactly "and that piece2 of art is useful"
+* main - prints exactly "and that piece of art is useful"
 * - Dora korpar, 2015-10-19",
 * followed by a new line, to the standard error.
 * Return: Always 1 (Success)
@@ -11,7 +11,7 @@
 int main(void)
 {
 
-write(2, "and that piece of art is useful\* - Dora korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 
 return (1);
 }
