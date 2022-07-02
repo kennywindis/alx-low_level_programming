dd6cc712371054c61caf0a81ca036442d0bd5e5b
kennywindis <kenyphilip100@gmail.com>
2022-07-02 11:14:07 -0700

    view

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 00 to 89
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+		int c, i;
+
+		for (c = '00'; c <= '89'; c++)
+{
+	for (i = '00'; i <= '89'; i++)
+	{
+		if (c < i)
+		{
+			putchar(c);
+
+			putchar(i);
+
+		if (c != '89' || (c == '88' && i != '89'))
+
+		{
+			putchar(',');
+
+			putchar(' ');
+		}
+		}
+	}
+}
+putchar('\n');
+
+return (0);
+}
