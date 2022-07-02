fe4afa058c736bfb977c0dd8c0a37514debe03a1
kennywindis <kenyphilip100@gmail.com>
2022-07-02 14:18:19 -0700

    view

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of 2-digit numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int c, i, k, j;
+
+for (c = 48; c <= 57; c++)
+{
+for (i = 48; i <= 57; i++)
+{
+for (k = 48; k <= 57; k++)
+{
+for (j = 48; j <= 57; j++)
+{
+if (((k + J) > (c + i) && k >= c) || c < k)
+{
+putchar(c);
+putchar(i);
+putchar('');
+putchar(k);
+putchar(j);
+
+if (c + i + j + k + j == 227 && c == 57)
+{
+break
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
