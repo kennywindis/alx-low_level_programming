2dbc46b9d837cdbd29dcb88488158e36859a4221
kennywindis <kenyphilip100@gmail.com>
2022-07-02 04:55:14 -0700

    view

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print base 16 numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n')
+		return (0);
+}
