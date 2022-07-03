fcf6fbd4e42f24d983fc68b2f208b01661dad11b
kennywindis <kenyphilip100@gmail.com>
2022-07-03 12:38:27 -0700

    view

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints all possible combination
@@ -11,22 +11,23 @@ int main(void)
 {
 int c, i, k, j;
 
-for (c = 00; c <= 57; c++)
+for (c = 48; c <= 57; c++)
 {
-for (i = 00; i <= 57; i++)
+for (i = 48; i <= 57; i++)
 {
-for (k = 00; k <= 57; k++)
+for (k = 48; k <= 57; k++)
 {
-for (j = 00; j <= 57; j++)
+for (j = 48; j <= 57; j++)
 {
 if (((k + j) > (c + i) && k >= c) || c < k)
 {
 putchar(c);
 putchar(i);
+putchar(' ');
 putchar(k);
 putchar(j);
 
-if (c + i + k + j == 28 && c == 57)
+if (c + i + k + j == 227 && c == 57)
 {
 break;
 }
