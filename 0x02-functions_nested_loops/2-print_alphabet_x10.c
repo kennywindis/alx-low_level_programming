01097378c3f621577e3120f08eca507bbec412d6
kennywindis <kenyphilip100@gmail.com>
2022-07-05 19:03:11 -0700

    fnl

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	char k;
+
+	int d = 0;
+
+	while (d <= 9)
+	{
+		for (k = 'a'; k <= 'z' k++)
+		{
+			_putchar(k);
+
+		}
+
+		_putchar('\n');
+
+		d++
+
+		}
+}
+
