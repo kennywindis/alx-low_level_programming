3a1ff5c694facd7dcf737e87d362c0936a522eb3
kennywindis <kenyphilip100@gmail.com>
2022-07-06 11:01:52 -0700

    fnl

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the times table with the parameter
+ * @n: parameter
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+
+int num, xply, result;
+
+if (n <= 15 && n >= 0)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+
+for (xply = 0; xply <= n; xply++)
+{
+_putchar(',');
+_putchar(' ');
+
+result = num * xply;
+if (result <= 99)
+_putchar(' ');
+
+if (result <= 9)
+_putchar(' ');
+
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
