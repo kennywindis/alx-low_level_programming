d41afb2f68a8884d2caff4cc36aa0b9c60e2fe0f
kennywindis <kenyphilip100@gmail.com>
2022-07-06 08:20:55 -0700

    fnl

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds_two_integers
+ * @d1:integer 1
+ * @d2:integer 2
+ * Return: sum
+ */
+
+int add(int d1, int d2)
+{
+
+int sum;
+
+sum = d1 + d2;
+
+return (sum);
+}
