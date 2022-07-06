f677eb31da65a136e5db88dabc1cd40b912d4e4e
kennywindis <kenyphilip100@gmail.com>
2022-07-05 19:11:38 -0700

    fnl

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	while (d <= 9)
 	{
-		for (k = 'a'; k <= 'z' k++)
+		for (k = 'a'; k <= 'z'; k++)
 		{
 			_putchar(k);
 
@@ -20,7 +20,7 @@ void print_alphabet_x10(void)
 
 		_putchar('\n');
 
-		d++
+		d++;
 
 		}
 }
