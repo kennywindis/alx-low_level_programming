56c37d8b4dbf4a4ed62279c8dd4c8ed63eef7961
kennywindis <kenyphilip100@gmail.com>
2022-07-06 18:48:37 -0700

    fnl

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int k;
-unsigned long n1 = 0, n2 = 1 n3;
+unsigned long n1 = 0, n2 = 1, n3;
 unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
 unsigned long h1, h2;
 
@@ -23,7 +23,7 @@ n1_h1 = n1 / 10000000000;
 n2_h1 = n2 / 10000000000;
 n1_h2 = n1 % 10000000000;
 n2_h2 = n2 % 10000000000;
-for (inc = 93; inc < 99; inc++)
+for (k = 93; k < 99; k++)
 {
 h1 = n1_h1 + n2_h1;
 h2 = n1_h2 + n2_h2;
