f687e88de497b5ff1ef24c33a97d3bdae64f8550
kennywindis <kenyphilip100@gmail.com>
2022-08-04 19:43:22 -0700

    vf

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,12 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
-
 
 /**
  * print_char - Prints a char.
@@ -99,6 +99,7 @@ i++;
 }
 printf("\n");
 va_end(args);
+
 }
 
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,8 @@
  */
 
 #include <stdarg.h>
+#include <stdio.h>
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
