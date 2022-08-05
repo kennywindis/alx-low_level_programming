101fb995a11ed56eb28e087c69be59eec3e033ae
kennywindis <kenyphilip100@gmail.com>
2022-08-04 17:31:47 -0700

    vf

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0F-variadic_functions directory.
+ */
+
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif
