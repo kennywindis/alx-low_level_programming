776d105483d3cb847a843721e6008c0eb37af23a
kennywindis <kenyphilip100@gmail.com>
2022-08-18 18:25:39 -0700

    bm

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x13-bit_manipulation directory.
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+#endif
