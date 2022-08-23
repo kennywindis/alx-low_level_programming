5710677f59de1c44f847e69e6a0734fbc9f858d1
kennywindis <kenyphilip100@gmail.com>
2022-08-22 18:53:24 -0700

    fi

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+
+
+
+
+#endif
