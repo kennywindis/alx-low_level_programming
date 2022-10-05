b4681b829ad1388fcc8a0a700286f18c8aba2495
kennywindis <kennyphilip100@gmail.com>
2022-10-04 17:34:18 -0700

    dl

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/kennywindis/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/kennywindis/alx-low_level_programming/
+master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
